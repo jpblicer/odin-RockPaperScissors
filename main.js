b1427b38c5069aa1746a4ea8b339254f7bbf0baa
jpblicer <jpblicer@gmail.com>
2023-10-11 23:01:18 +0900

    add if else logic to getComputerChoice to return a random selection

--- main.js
+++ main.js
@@ -6,13 +6,24 @@ function getRandomInt(max = 3){
     return Math.floor(Math.random()*max)
 }
 
-console.log(getRandomInt())
 
-/*
-const computerSelection = getComputerChoice(){
-
+function getComputerChoice(getRandomInt){
+    if(getRandomInt === 0){
+        console.log("computer Rock")
+    }else if(getRandomInt === 1){
+        console.log("computer Paper")
+    }else if(getRandomInt === 2){
+        console.log("computer Scissors")
+    }else{
+        console.error("getComputerChoice failed")
+    }
 }
 
+
+const computerSelection = getComputerChoice(getRandomInt())
+
+
+/*
 function playRound(playerSelection, computerSelection){
 
 }
